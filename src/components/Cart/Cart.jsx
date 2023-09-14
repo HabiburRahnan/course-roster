64862d22449a98857a211e2169ffb174b629a1fa
unknown <habiburrahmanofficial157@gmail.com>
2023-09-15 00:31:49 +0600

    button a seup onclick and set usestate and show data and data pass cart section

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,15 @@
-const Cart = () => {
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
+const Cart = ({ allCredit }) => {
   return (
     <div>
       <div>
-        <h1 className="text-xl text-center font-bold">Cart Section</h1>
+        <h3 className="font-semibold my-4">Credit Hour Remaining: </h3>
+        <h3 className="text-xl font-bold my-4">Course Name: </h3>
+        {allCredit.map((item) => (
+          <li className="list-decimal ml-6 text-lg font-medium" key={item.id}>{item.course_name}</li>
+        ))}
+        <h3 className="text-xl font-semibold my-4">Total Credit Hour: </h3>
       </div>
     </div>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,25 +4,30 @@ import { BsBook } from "react-icons/bs";
 
 const Home = () => {
   const [registrationCard, setRegistrationCard] = useState([]);
+  const [allCredit, setAllCredit] = useState([]);
 
   useEffect(() => {
-    fetch(`../course.json`)
+    fetch(`./course.json`)
       .then((res) => res.json())
       .then((data) => setRegistrationCard(data));
   }, []);
 
+  const handleSelectItem = (item) => {
+    setAllCredit([...allCredit, item]);
+  };
+  console.log(allCredit);
   return (
     <div>
       {/* main container */}
-      <div className="flex ">
+      <div className="flex justify-around ">
         {/* Card container */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-6 mx-5">
+        <div className="w-3/4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-6 mx-5">
           {/* card */}
 
           {registrationCard.map((item) => (
             <div
               key={item.id}
-              className="card card-compact  bg-base-100 shadow-xl mt-6 ">
+              className="card card-compact   bg-base-100 shadow-xl mt-6 ">
               <img className="rounded-2xl" src={item.image} alt="Image" />
               <div className="card-body">
                 <h2 className="card-title ">{item.course_name}</h2>
@@ -31,7 +36,7 @@ const Home = () => {
                   by the readable content of a page when looking at its layout.
                 </p>
                 {/* price and credit section */}
-                <div className="text-base flex">
+                <div className="text-lg flex">
                   <p>$ Price:{item.price}</p>
                   <div className="flex items-center gap-2">
                     <BsBook></BsBook>
@@ -39,14 +44,18 @@ const Home = () => {
                   </div>
                 </div>
                 <div className="card-actions justify-center">
-                  <button className="btn btn-primary w-full">Select</button>
+                  <button
+                    onClick={() => handleSelectItem(item)}
+                    className="btn btn-primary w-full">
+                    Select
+                  </button>
                 </div>
               </div>
             </div>
           ))}
         </div>
         <div className="w-1/3">
-          <Cart></Cart>
+          <Cart allCredit={allCredit}></Cart>
         </div>
       </div>
     </div>
