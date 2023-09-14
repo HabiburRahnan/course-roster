701c195cb7d395574255bf0051dd45bc24ed7663
unknown <habiburrahmanofficial157@gmail.com>
2023-09-15 00:38:12 +0600

    home thaki pass kora data ar opr map kore item ber kore select button a click korla cart section a course name ar name show korbe

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,10 +4,12 @@ const Cart = ({ allCredit }) => {
   return (
     <div>
       <div>
-        <h3 className="font-semibold my-4">Credit Hour Remaining: </h3>
+        <h3 className="font-semibold my-4">Credit Hour Remaining: {20} </h3>
         <h3 className="text-xl font-bold my-4">Course Name: </h3>
         {allCredit.map((item) => (
-          <li className="list-decimal ml-6 text-lg font-medium" key={item.id}>{item.course_name}</li>
+          <li className="list-decimal ml-6 text-lg font-medium" key={item.id}>
+            {item.course_name}
+          </li>
         ))}
         <h3 className="text-xl font-semibold my-4">Total Credit Hour: </h3>
       </div>
