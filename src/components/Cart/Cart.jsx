9c656a27ddf5376d22fcb9e4f17871645efeba12
unknown <habiburrahmanofficial157@gmail.com>
2023-09-16 19:31:12 +0600

    credit and remaining credit a hour set

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,17 +3,17 @@ const Cart = ({ allCredit, remaining, totalHour }) => {
   return (
     <div>
       <div>
-        <h3 className="font-semibold my-4">
-          Credit Hour Remaining: {remaining}
+        <h3 className="text-xl font-semibold my-4">
+          Credit Hour Remaining: {remaining} hrs
         </h3>
-        <h3 className="text-xl font-bold my-4">Course Name</h3>
+        <h3 className="text-2xl font-bold my-4">Course Name</h3>
         {allCredit.map((item) => (
           <li key={item.id} className="list-decimal ml-6 text-lg font-medium">
             {item.course_name}
           </li>
         ))}
         <h3 className="text-xl font-semibold my-4">
-          Total Credit Hour:{totalHour}
+          Total Credit Hour:{totalHour} hrs
         </h3>
       </div>
     </div>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -56,7 +56,7 @@ const Home = () => {
               <img className="rounded-2xl" src={item.image} alt="Image" />
               <div className="card-body">
                 <h2 className="card-title ">{item.course_name}</h2>
-                <p className="">{item.details.slice(0,120)}</p>
+                <p className="">{item.details.slice(0, 120)}</p>
                 {/* price and credit section */}
                 <div className="text-lg flex">
                   <p>$ Price:{item.price}</p>
